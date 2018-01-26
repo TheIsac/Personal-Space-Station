a17e36eef5683f14a441658a51600ba1bbb118bc
josefina <31647229+Raseration@users.noreply.github.com>
2018-01-26 14:17:17 +0100

    Input... again
    Fixed the input again

---
+++ PersonalSpaceStation/Assets/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    //Variables
+    [HideInInspector] public float horizontalAxis;
+    [HideInInspector] public float verticalAxis;
+    public string player;
+    public float moveSpeed;
+
+    private Vector3 movementDirection;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalAxis = Input.GetAxis("Horizontal" + player);
+        verticalAxis = Input.GetAxis("Vertical" + player);
+        CharacterMovement();
+    }
+
+    public void CharacterMovement()
+    {
+        movementDirection = new Vector3(horizontalAxis, 0.0f, verticalAxis);
+        transform.position += movementDirection * moveSpeed;
+        Quaternion.LookRotation(movementDirection);
+    }
+}
+
+
+
+
+
