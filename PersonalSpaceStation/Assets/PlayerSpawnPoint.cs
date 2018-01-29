13170b746352c4b1d97354ad7a3e61997f5768eb
Jinexsus <31748139+Jinexsus@users.noreply.github.com>
2018-01-29 14:54:43 +0100

    Spawner
    Spawnpoint

---
+++ PersonalSpaceStation/Assets/PlayerSpawnPoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawnPoint : MonoBehaviour {
+    public GameObject Player;
+    public float repeatTime = 3f;
+
+    void Start()
+    {
+        Invoke("Spawn", 2f);
+    }
+
+    void Spawn()
+    {
+        Instantiate(Player, transform.position, Quaternion.identity);
+    }
+}
---
+++ PersonalSpaceStation/Assets/spawning.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawning : MonoBehaviour {
+
+    public Transform prefab;
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(prefab, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
