cbe4d13501be49828d111dd45f0db7d6752bc1bd
josefina <31647229+Raseration@users.noreply.github.com>
2018-01-30 14:00:47 +0100

    Changed the name to a better one

--- PersonalSpaceStation/Assets/Scripts/PackageHandler.cs
+++ PersonalSpaceStation/Assets/Scripts/PackageHandler.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PackageHandling : MonoBehaviour
+public class PackageHandler : MonoBehaviour
 {
 
     public Interactable[] stations;
