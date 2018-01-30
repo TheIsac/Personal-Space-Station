32e708c2eae9cb06f96032654bb1b89acb7b3637
Fisac <isac_degerborg@hotmail.com>
2018-01-30 14:20:51 +0100

    Spawner
    Spawner editor

--- PersonalSpaceStation/Assets/Scripts/Spawner.cs
+++ PersonalSpaceStation/Assets/Scripts/Spawner.cs
@@ -2,24 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour {
+public class Spawner : MonoBehaviour
+{
 
     public GameObject player;
     private Vector3 currentPos;
     private Quaternion currentQuat;
 
+    void Start()
+    {
 
-	void Start () {
         currentPos = gameObject.transform.position;
         currentQuat = Quaternion.Euler(0, 0, 0);
 
-        if(player != null)
+        if (player != null)
         {
             Instantiate(player, currentPos, currentQuat);
         }
-	}
+    }
 
-    void OnDrawGizmos() {
+    void OnDrawGizmos()
+    {
         Gizmos.color = new Color(0, 1, 0, 0.5F);
         Gizmos.DrawCube(transform.position, new Vector3(1, 1, 1));
     }
---
+++ PersonalSpaceStation/Assets/Scripts/SpawnerEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Spawner))]
+public class SpawnerEditor : Editor
+{
+    string[] options = new string[] { "P1", "P2", "P3", "P4", };
+    public int index = 0;
+
+    public override void OnInspectorGUI()
+    {
+        index = EditorGUILayout.Popup("Player", index, options);
+    }
+}
