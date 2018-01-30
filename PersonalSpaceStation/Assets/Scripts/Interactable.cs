818e6d4a9085419d8920cca5d86af9ba013a39bd
Demonsid <niklas@lingsell.se>
2018-01-30 11:28:54 +0100

    Engine Room
    Added Engine health

--- PersonalSpaceStation/Assets/Scripts/Interactable.cs
+++ PersonalSpaceStation/Assets/Scripts/Interactable.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Interactable : MonoBehaviour {
 
@@ -8,10 +10,23 @@ public class Interactable : MonoBehaviour {
     public bool inUse = false;
     private bool playerInRange = false;
     public bool isWorking;
+    private int stationHealth = 50;
+    public string stationName = "Engine Room";
+
+    public Action OnStationFailure;
+    public Action OnStationFixed;
+
+    private float lastTick;
+    public float tickLength = 2F;
+
+    public int minWorkingHealth = 25;
+    public int maxWorkingHealth = 75;
+
+    public Text healthtext;
 
     void Start()
     {
-
+        lastTick = Time.time;
     }
 
     void Update()
@@ -20,6 +35,75 @@ public class Interactable : MonoBehaviour {
         {
             HandlePlayerInput();
         }
+
+        Tick();
+    }
+
+    public void MiniGameComplete()
+    {
+        miniGame.SetActive(false);
+    }
+
+    public void AddHealthToStation(int healthToGive)
+    {
+        stationHealth += healthToGive;
+        UpdateHealthDisplay();
+    }
+
+    private void UpdateHealthDisplay()
+    {
+        healthtext.text = stationHealth.ToString();
+    }
+
+    void Tick()
+    {
+        if(Time.time - lastTick > tickLength)
+        {
+            lastTick = Time.time;
+            stationHealth--;
+
+            UpdateHealthDisplay();
+            CheckStationHealth();
+        }
+    }
+
+    void CheckStationHealth()
+    {
+        if ((stationHealth < minWorkingHealth || stationHealth > maxWorkingHealth) && isWorking)
+        {
+            StationFailure();
+        }
+
+        if (stationHealth <= maxWorkingHealth && stationHealth >= minWorkingHealth && !isWorking)
+        {
+            StationFixed();
+        }
+    }
+
+    void StationFailure()
+    {
+        Debug.Log("ZOMG ENGINE BORKED!");
+        isWorking = false;
+
+        if (OnStationFailure != null)
+        {
+            OnStationFailure.Invoke();
+        }
+
+        healthtext.color = Color.red;
+    }
+
+    void StationFixed()
+    {
+        Debug.Log("LOL FIXERD");
+        isWorking = true;
+
+        if (OnStationFixed != null)
+        {
+            OnStationFixed.Invoke();
+        }
+
+        healthtext.color = Color.black;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -45,6 +129,7 @@ public class Interactable : MonoBehaviour {
             {
                 inUse = true;
                 miniGame.SetActive(true);
+                miniGame.GetComponent<EngineMiniGame>().ResetStation();
             }
 
         }
