60be4fc0ab4af5ca8211308f07e736e403aed60b
Jinexsus <31748139+Jinexsus@users.noreply.github.com>
2018-01-30 12:21:39 +0100

    Door animation and Camera Control
    Camera control and animation for the door

---
+++ PersonalSpaceStation/Assets/Door.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    Animator animator;
+    bool doorOpen;
+
+    void Start()
+    {
+        doorOpen = false;
+        animator = GetComponent<Animator>();
+    }
+
+     void OnTriggerEnter(Collider col)
+    {
+      if(col.gameObject.tag == "player")
+        {
+            doorOpen = true;
+            DoorControl("Open");
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (doorOpen)
+        {
+            doorOpen = false;
+            DoorControl("Close");
+        }
+    }
+
+    void DoorControl(string direction)
+        {
+        animator.SetTrigger(direction);
+        }
+    
+        
+    
+}
