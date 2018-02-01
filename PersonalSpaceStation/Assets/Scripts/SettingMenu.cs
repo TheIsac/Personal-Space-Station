b713673093dfbf4d60644888924fbac7f8a10d90
Jinexsus <31748139+Jinexsus@users.noreply.github.com>
2018-02-01 14:58:09 +0100

    Audio
    Audio adjust in the settings menu(options)

---
+++ PersonalSpaceStation/Assets/Scripts/SettingMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class SettingMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+    
+
+	public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("Music", volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFX", volume);
+    }
+}
