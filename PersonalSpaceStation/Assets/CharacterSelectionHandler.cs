9b122b193fd879f29847ccc0ce10064bac6683c7
josefina <31647229+Raseration@users.noreply.github.com>
2018-02-02 12:17:23 +0100

    Fixed the character selection stuffs

--- PersonalSpaceStation/Assets/CharacterSelectionHandler.cs
+++ PersonalSpaceStation/Assets/CharacterSelectionHandler.cs
@@ -14,14 +14,24 @@ public class CharacterSelectionHandler : MonoBehaviour {
     private bool playerJoined3 = false;
     private bool playerJoined4 = false;
     private int numberOfPlayers;
+    public Button playerReady1;
+    public Button playerReady2;
+    public Button playerReady3;
+    public Button playerReady4;
+    public bool player1Ready;
+    public bool player2Ready;
+    public bool player3Ready;
+    public bool player4Ready;
 
-    public Button start;
+    //public Text start;
     public Image playerSelectBackground;
     public Button selectCharacters;
 
-    public Button[] playerButton;
-    public Image[] playerColor;
-    public Text[] playerText;
+    public GameObject[] player;
+    //public Button[] playerButton;
+    //public Image[] playerColor;
+    //public Text[] playerText;
+    public GameObject[] selectCharacter;
 
 	// Use this for initialization
 	void Start ()
@@ -40,38 +50,99 @@ public class CharacterSelectionHandler : MonoBehaviour {
             if (Input.GetButtonDown("A-button_P1") && playerSelect == false)
             {
                 playerSelect = true;
+                //start.gameObject.SetActive(false);
+            }
+
+            if (Input.GetButtonDown("B-button_P1") && numberOfPlayers == 0)
+            {
+                //leave the character selection screen.
             }
 
             if (playerSelect == true)
             {
+            //Player One
                 if (Input.GetButtonDown("A-button_P1") && playerJoined1 == false)
                 {
                     PlayerJoin(0);
                     playerJoined1 = true;
+                    player[0].gameObject.SetActive(true);
                 }
+                if (Input.GetButtonDown("X-button_P1") && playerJoined1 == true)
+                {
+                    playerReady1.gameObject.SetActive(true);
+                    player1Ready = true;
+                }
+                if (Input.GetButtonDown("B-button_P1") && playerJoined1 == true)
+                {
+                    playerReady1.gameObject.SetActive(false);
+                    player1Ready = false;
+                }
+
+            //Player Two
                 if (Input.GetButtonDown("A-button_P2") && playerJoined1 == false)
                 {
                     PlayerJoin(1);
                     playerJoined2 = true;
+                    player[1].gameObject.SetActive(true);
                 }
+                if (Input.GetButtonDown("X-button_P2") && playerJoined1 == true)
+                {
+                    playerReady2.gameObject.SetActive(true);
+                    player2Ready = true;
+                }
+                if (Input.GetButtonDown("B-button_P2") && playerJoined1 == true)
+                {
+                    playerReady2.gameObject.SetActive(false);
+                    player2Ready = false;
+                }
+            //Player Three
                 if (Input.GetButtonDown("A-button_P3") && playerJoined1 == false)
                 {
                     PlayerJoin(2);
                     playerJoined3 = true;
+                    player[2].gameObject.SetActive(true);
                 }
+                if (Input.GetButtonDown("X-button_P3") && playerJoined1 == true)
+                {
+                    playerReady3.gameObject.SetActive(true);
+                    player3Ready = true;   
+                }
+                if (Input.GetButtonDown("B-button_P3") && playerJoined1 == true)
+                {
+                    playerReady3.gameObject.SetActive(false);
+                    player3Ready = false;
+                }
+            //Player Four
                 if (Input.GetButtonDown("A-button_P4") && playerJoined1 == false)
                 {
                     PlayerJoin(3);
                     playerJoined4 = true;
+                    player[3].gameObject.SetActive(true);
                 }
-            }
+                if (Input.GetButtonDown("X-button_P4") && playerJoined1 == true)
+                {
+                    playerReady4.gameObject.SetActive(true);
+                    player4Ready = true;
+                }
+                if (Input.GetButtonDown("B-button_P4") && playerJoined1 == true)
+                {
+                    playerReady4.gameObject.SetActive(false);
+                    player4Ready = false;
+                }
         }
+    }
 
     public void PlayerJoin(int index)
     {
-        playerButton[index].gameObject.SetActive(false);
-        playerColor[index].gameObject.SetActive(true);
-        playerText[index].gameObject.SetActive(true);
-        numberOfPlayers += 1;
+        if (index > 4)
+        {
+            return;
+        }
+        
+            //playerButton[index].gameObject.SetActive(false);
+            //playerColor[index].gameObject.SetActive(true);
+            //playerText[index].gameObject.SetActive(true);
+            selectCharacter[index].gameObject.SetActive(true);
+            numberOfPlayers += 1;
     }
 }
---
+++ PersonalSpaceStation/Assets/Ready.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ready : MonoBehaviour {
+
+    public bool playerReady1 = false;
+    public bool playerReady2 = false;
+    public bool playerReady3 = false;
+    public bool playerReady4 = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
