39d15c192f5143d658c7cc05ac6c0d3debe6d752
Fisac <isac_degerborg@hotmail.com>
2018-02-05 15:42:35 +0100

    pump minigame
    now you must fully release one button before you can press the second one.

--- PersonalSpaceStation/Assets/Scripts/PumpMiniGame.cs
+++ PersonalSpaceStation/Assets/Scripts/PumpMiniGame.cs
@@ -63,14 +63,14 @@ public class PumpMiniGame : MonoBehaviour, IResetUser, IResetStation
         if (stationUser == "")
             return;
 
-        if (Input.GetButtonDown("X-button" + stationUser) && buttonXLastPressed == false)
+        if (Input.GetButtonDown("X-button" + stationUser) && buttonXLastPressed == false && !Input.GetButton("Y-button" + stationUser))
         {
             completionCounter += clickvalue;
             buttonXLastPressed = true;
             UpdateGauge();
         }
 
-        if (Input.GetButtonDown("Y-button" + stationUser) && buttonXLastPressed == true)
+        if (Input.GetButtonDown("Y-button" + stationUser) && buttonXLastPressed == true && !Input.GetButton("X-button" + stationUser))
         {
             completionCounter += clickvalue;
             buttonXLastPressed = false;
