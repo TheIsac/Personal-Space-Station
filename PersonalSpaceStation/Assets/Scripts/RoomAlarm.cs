ace20e24897d1b24dfe3849209c5490ac482b576
Fisac <isac_degerborg@hotmail.com>
2018-02-06 11:34:31 +0100

    Lightstuff

---
+++ PersonalSpaceStation/Assets/Scripts/RoomAlarm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomAlarm : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject station;
+
+    private void Start()
+    {
+        station.GetComponent<Interactable>().OnStationFailure += OnEngineFailure;
+        station.GetComponent<Interactable>().OnStationFixed += OnEngineFixed;
+    }
+
+    void OnEngineFailure()
+    {
+        ActivateAlarm();
+    }
+
+    void OnEngineFixed()
+    {
+        DeactivateAlarm();
+    }
+
+    private void ActivateAlarm()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+    }
+
+    private void DeactivateAlarm()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+}
