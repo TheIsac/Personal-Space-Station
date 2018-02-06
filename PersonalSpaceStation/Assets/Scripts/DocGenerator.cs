0171bf812e26fd6f953dae6b4e0f4b395409738e
Joakim <jokenordstrom@hotmail.com>
2018-02-06 11:18:12 +0100

    Document delivery
    Almost done.

--- PersonalSpaceStation/Assets/Scripts/DocGenerator.cs
+++ PersonalSpaceStation/Assets/Scripts/DocGenerator.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class DocGenerator : MonoBehaviour {
 
+    public Station currentStation;
+
     public bool courier = false;
 
     public int successCounter = 0;
@@ -11,9 +13,15 @@ public class DocGenerator : MonoBehaviour {
     public GameObject documents;
     public Transform docSpawnPoint;
 
+    public Document document;
+
+    public Interactable myStation;
+
 	// Use this for initialization
 	void Start () {
 
+        //listen to HandIn
+        document.HandIn += UnLockStation;
     }
 
     //recieve signal from Interactable, that a station was fixed and count the success
@@ -28,20 +36,35 @@ public class DocGenerator : MonoBehaviour {
             successCounter = 0;
 
             SpawnPaperWork();
+            LockStation();
 
             successCounter += 1;
         }
     }
     public void SpawnPaperWork()
     {
-        Instantiate(documents, docSpawnPoint);
+        GameObject NewDocument = Instantiate(documents, docSpawnPoint);
+        NewDocument.GetComponent<Document>().SetDestinationStation(Station.EngineRoom);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        //if(/*player carries a document of right type*/)
-        //{
-        //    courier = true;
-        //}
+        if (/*player with RIGHT document collide with trigger zone*/)
+        {
+            if (/*document dropped*/)
+            {
+                GetComponent<Document>().DeliverDocument();
+            }
+        }
+    }
+
+    public void LockStation()
+    {
+        myStation.inUse = true;
+    }
+
+    public void UnLockStation()
+    {
+        myStation.inUse = false;
     }
 }
