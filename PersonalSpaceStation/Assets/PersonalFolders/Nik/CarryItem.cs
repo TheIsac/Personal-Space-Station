63af6fac56c0a611f0e5650e3e25d03c36e37cfe
Demonsid <niklas@lingsell.se>
2018-02-06 12:46:28 +0100

    Carry Items
    Basic functionality to pick up, carry and drop items

---
+++ PersonalSpaceStation/Assets/PersonalFolders/Nik/CarryItem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarryItem : MonoBehaviour {
+
+    public bool isBeingCarried = false;
+    public BoxCollider boxCollider;
+
+    private Transform originalParent;
+
+	void Start () {
+        originalParent = transform.parent;
+
+    }
+	
+	void Update () {
+		
+	}
+
+    public void PickUp(Transform carryPoint)
+    {
+        transform.position = carryPoint.position;
+        transform.rotation = carryPoint.rotation;
+        transform.SetParent(carryPoint);
+        isBeingCarried = true;
+
+        if(boxCollider != null)
+            boxCollider.enabled = false;
+    }
+
+    public void Drop()
+    {
+        transform.SetParent(originalParent);
+        isBeingCarried = false;
+
+        if (boxCollider != null)
+            boxCollider.enabled = true;
+    }
+}
---
+++ PersonalSpaceStation/Assets/PersonalFolders/Nik/ItemHandler.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemHandler : MonoBehaviour {
+
+    //public bool itemInRange = false;
+    private Movement movement;
+    CarryItem carriedItem = null;
+    CarryItem itemInRange = null;
+
+    public Transform carryPoint;
+
+    private void Awake()
+    {
+        movement = GetComponent<Movement>();
+    }
+
+    private void Update()
+    {
+        if (movement == null)
+            return;
+
+
+        if(itemInRange || carriedItem)
+        {
+            PlayerInput();
+        }
+    }
+
+    void PlayerInput()
+    {
+        if(carriedItem == null)
+        {
+            if (Input.GetButtonDown("A-button" + movement.player))
+            {
+                carriedItem = itemInRange;
+
+                if (carryPoint != null)
+                    carriedItem.PickUp(carryPoint);
+                else
+                    carriedItem.PickUp(transform);
+            }
+        }
+        else
+        {
+            if (Input.GetButtonDown("A-button" + movement.player))
+            {
+                carriedItem.Drop();
+                itemInRange = carriedItem;
+                carriedItem = null;
+            }
+        }
+
+
+
+
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CarryItem tempItem = other.GetComponent<CarryItem>();
+
+        if (tempItem != null)
+        {
+            itemInRange = tempItem;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        CarryItem tempItem = other.GetComponent<CarryItem>();
+
+        if (tempItem != null)
+        {
+            itemInRange = null;
+        }
+    }
+
+    //public void SetItemInRange(bool inRange)
+    //{
+    //    itemInRange = inRange;
+    //}
+}
