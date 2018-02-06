53b194a3b33f9f462aaa9f7e2cf8a89656fbda60
Demonsid <niklas@lingsell.se>
2018-02-06 14:01:51 +0100

    push
    sdf

--- PersonalSpaceStation/Assets/Scripts/DocGenerator.cs
+++ PersonalSpaceStation/Assets/Scripts/DocGenerator.cs
@@ -12,24 +12,24 @@ public class DocGenerator : MonoBehaviour {
     public GameObject documents;
     public Transform docSpawnPoint;
 
-    public Document document;
+    //public Document document;
 
     public Interactable myStation;
 
 	// Use this for initialization
 	void Start () {
-
-        //listen to HandIn
-        document.HandIn += UnLockStation;
+   
     }
 
     //recieve signal from Interactable, that a station was fixed and count the success
     public void DocumentGenerator()
     {
+        Debug.Log(successCounter);
         if (successCounter < 3)
         {
             successCounter += 1;
         }
+        //if successCounter reaches 3, spawnPaperWork and lock this station
         else if (successCounter == 3)
         {
             successCounter = 0;
@@ -42,14 +42,18 @@ public class DocGenerator : MonoBehaviour {
     }
     public void SpawnPaperWork()
     {
+        //randomize target station for spawned document, while target is this station, randomize again
         do
         {
             targetStation = (Station)Random.Range(0, System.Enum.GetValues(typeof(Station)).Length - 1);
         }
         while (currentStation == targetStation);
-
+        //initiate a new document with the new target
         GameObject NewDocument = Instantiate(documents, docSpawnPoint);
         NewDocument.GetComponent<Document>().SetDestinationStation(targetStation);
+
+        //listen to HandIn
+        NewDocument.GetComponent<Document>().HandIn += UnLockStation;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -63,11 +67,11 @@ public class DocGenerator : MonoBehaviour {
 
     public void LockStation()
     {
-        myStation.inUse = true;
+        myStation.locked = true;
     }
 
     public void UnLockStation()
     {
-        myStation.inUse = false;
+        myStation.locked = false;
     }
 }
