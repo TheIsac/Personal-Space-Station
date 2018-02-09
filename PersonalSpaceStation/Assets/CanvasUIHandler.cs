28b021330aac8e2dc99e9a0b26413821fe39db15
Fisac <isac_degerborg@hotmail.com>
2018-02-09 09:32:34 +0100

    changes to UI

--- PersonalSpaceStation/Assets/CanvasUIHandler.cs
+++ PersonalSpaceStation/Assets/CanvasUIHandler.cs
@@ -5,60 +5,70 @@ using UnityEngine.UI;
 
 public class CanvasUIHandler : MonoBehaviour {
 
-    public GameObject engine, pump, atmo, plant;
+    public Interactable engine, pump, atmo, plant;
 
     public Image aEngine, aPump, aAtmo, aPlant;
     public Image eEngine, ePump, eAtmo, ePlant;
+    public Image docEngine, docPump, docAtmo, docPlant;
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (engine.GetComponent<Interactable>().locked)
+        if (engine.locked)
         {
             aEngine.color = Color.clear;
             eEngine.color = Color.white;
+            docEngine.color = Color.white;
         }
         else
         {
             aEngine.color = Color.white;
             eEngine.color = Color.clear;
+            docEngine.color = Color.clear;
         }
 
-        if (pump.GetComponent<Interactable>().locked)
+        if (pump.locked)
         {
             aPump.color = Color.clear;
             ePump.color = Color.white;
+            docPump.color = Color.white;
         }
         else
         {
             aPump.color = Color.white;
             ePump.color = Color.clear;
+            docPump.color = Color.clear;
         }
 
-        if (atmo.GetComponent<Interactable>().locked)
+        if (atmo.locked)
         {
             aAtmo.color = Color.clear;
             eAtmo.color = Color.white;
+            docAtmo.color = Color.white;
         }
         else
         {
             aAtmo.color = Color.white;
             eAtmo.color = Color.clear;
+            docAtmo.color = Color.clear;
         }
 
-        if (plant.GetComponent<Interactable>().locked)
+        if (plant.locked)
         {
             aPlant.color = Color.clear;
             ePlant.color = Color.white;
+            docPlant.color = Color.white;
         }
         else
         {
             aPlant.color = Color.white;
             ePlant.color = Color.clear;
+            docPlant.color = Color.clear;
         }
     }
 }
