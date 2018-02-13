c41fa579c1f6afc94100744fc6b44df2936272b8
Jinexsus <31748139+Jinexsus@users.noreply.github.com>
2018-02-13 15:11:28 +0100

    yos

--- PersonalSpaceStation/Assets/Scripts/MainMenu.cs
+++ PersonalSpaceStation/Assets/Scripts/MainMenu.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
+
+    public Text points;
 
 	public void PlayGame()
     {
         //Load next scene in the scene build order
-        SceneManager.LoadScene("CharacterSelect");
+        SceneManager.LoadScene("MainGame");
     }
 
     public void QuitGame()
     {
         Application.Quit();
     }
-
+    public void ShowHighScore()
+    {
+        points.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+    }
 }
