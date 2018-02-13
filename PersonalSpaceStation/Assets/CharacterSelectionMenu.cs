7e837a11ab1fc3a225552011ecb0e199eddd081e
josefina <31647229+Raseration@users.noreply.github.com>
2018-02-13 14:11:43 +0100

    Character selection!

--- PersonalSpaceStation/Assets/CharacterSelectionMenu.cs
+++ PersonalSpaceStation/Assets/CharacterSelectionMenu.cs
@@ -5,15 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class CharacterSelectionMenu : MonoBehaviour {
 
+    // public Character[] characters;
+    //public Vector3 playerSpawnPosition = new Vector3(0, 1, -7);
+   // public GameObject player;
+
+
     // Use this for initialization
     public void PlayGame()
     {
         //Load next scene in the scene build order
         SceneManager.LoadScene("IsacSpeltest");
+       // GameObject spawnedPlayer = Instantiate(player, playerSpawnPosition, Quaternion.identity) as GameObject;
+        //Character selectedCharacter = characters[characterChoice];
     }
-    // Update is called once per frame
+    
     public void BacktoMenu()
     {
+        //If the back button is pressed you go back to the Main Menu.
         SceneManager.LoadScene("Main menu");
     }
 }
