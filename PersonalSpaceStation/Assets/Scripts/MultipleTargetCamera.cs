97263c3cfd52b0f2ac99a5495e56d59740f3eb19
josefina <31647229+Raseration@users.noreply.github.com>
2018-02-15 15:45:08 +0100

    Revert "Merge branch 'master' of https://github.com/Fisac/Personal-Space-Station"
    This reverts commit f30ca120075187b02f2e073fe70001c36d9c07a7, reversing
    changes made to 303e6a418ca04648b64b7db6042a69333ef4286c.

---
+++ PersonalSpaceStation/Assets/Scripts/MultipleTargetCamera.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleTargetCamera : MonoBehaviour
+{
+    public List<Transform> targets;
+    public float smoothTime = .5f;
+
+    private Vector3 velocity;
+    private Vector3 cameraTarget;
+    public Vector3 offset;
+
+    private Transform target;
+
+    void Start()
+    {
+        targets = GameObject.FindGameObjectWithTag("Player").transform;
+        cameraTarget = new Vector3(targets.position.x, transform.position.y, target.position.z);
+    }
+
+    void LateUpdate()
+    {
+        if (targets.Count == 0)
+        {
+            return;
+        }
+
+        Vector3 centerpoint = GetCenterPoint();
+
+        Vector3 newPosition = centerpoint + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+    //gets the position of the players and finds the middle point of them to keep the camera in the center of them and keep every character in the range of the camera
+    Vector3 GetCenterPoint()
+    {
+        if (targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        return bounds.center;
+    }
+}
--- PersonalSpaceStation/Assets/Scripts/Scoring.cs
+++ PersonalSpaceStation/Assets/Scripts/Scoring.cs
@@ -1,1 +1,87 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoring : MonoBehaviour
+{
+
+    public float scoreTimer = 0;
+    public int totalScore = 0;
+    public Text scoreText;
+    private int scoreUpdateInterval = 6;
+    public float scoreMultiplier = 1;
+
+    public Interactable[] stations;
+
+    private int runningStations;
+    private int oldHighScore;
+
+
+    private void Start()
+    {
+        for (int i = 0; i < stations.Length; i++)
+        {
+            stations[i].OnStationFailure += OnStationFailure;
+            stations[i].OnStationFixed += OnStationRunning;
+        }
+        oldHighScore = PlayerPrefs.GetInt("HighScore", 0);
+    }
+
+    void Update()
+    {
+        if (Time.frameCount % scoreUpdateInterval == 0)
+        {
+            UpdateMultiplyer();
+        }
+        scoreTimer += Time.deltaTime * scoreMultiplier;
+        totalScore = (int)scoreTimer;
+
+        // Save the high score data if the score is indeed higher than the previous one
+        if (totalScore > oldHighScore)
+        {
+            PlayerPrefs.SetInt("HighScore", totalScore);
+            oldHighScore = totalScore;
+        }
+
+        scoreText.text = totalScore.ToString();
+    }
+
+    //Add score based on number of runningStations
+    void UpdateMultiplyer()
+    {
+        if (runningStations==1)
+        {
+            scoreMultiplier = 1f;
+        }
+        else if(runningStations == 2)
+        {
+            scoreMultiplier = 1.5f;
+        }
+        else if (runningStations == 3)
+        {
+            scoreMultiplier = 2.5f;
+        }
+        else if (runningStations == 4)
+        {
+            scoreMultiplier = 4f;
+        }
+
+    }
+    void OnStationFailure()
+    {
+        //Remove station from runningStations
+        if (runningStations != 1)
+        {
+            runningStations -= 1;
+        }
+    }
+    void OnStationRunning()
+    {
+        //Add station to runningStations
+        if (runningStations != 4)
+        {
+            runningStations += 1;
+        }
+    }
+}
