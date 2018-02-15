5edcdfc102f1f8ac75b84fe58f053ecd58544d85
Joakim <jokenordstrom@hotmail.com>
2018-02-15 11:03:06 +0100

    Fixed logo on documents
    fdhgfjsru

---
+++ PersonalSpaceStation/Assets/Scripts/SpritePositioner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpritePositioner : MonoBehaviour {
+
+    private bool continousUpdate = false;
+
+    void Start()
+    {
+        transform.position = transform.TransformPoint(0, 2, 0);
+    }
+
+    private void LateUpdate()
+    {
+        if (continousUpdate)
+        {
+            transform.rotation = Camera.main.transform.rotation;
+            transform.position = transform.TransformPoint(0, 2, 0);
+        }
+    }
+}
