3e54a101649cbbb07116bc14d904c33cc217f804
Joakim <jokenordstrom@hotmail.com>
2018-02-16 09:14:36 +0100

    Added "hard" pause
    lkj

---
+++ PersonalSpaceStation/Assets/Scripts/HardPause.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HardPause : MonoBehaviour {
+
+    private bool gameIsPaused = false;
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (GameManager.instance.gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    void Pause()
+    {
+        Time.timeScale = 0f;
+        GameManager.instance.gameIsPaused = true;
+    }
+
+    void Resume()
+    {
+        Time.timeScale = 1f;
+        GameManager.instance.gameIsPaused = false;
+    }
+}
