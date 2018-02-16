480a494195e8065ee4ce619fd1f3f631e315ccef
josefina <31647229+Raseration@users.noreply.github.com>
2018-02-16 13:44:01 +0100

    Komments!!

--- PersonalSpaceStation/Assets/Scripts/ButtonManager.cs
+++ PersonalSpaceStation/Assets/Scripts/ButtonManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
-
+    //changes scene if the player 1 presses the New Game button.
     public void NewGameBtn(string newGameLevel)
     {
         SceneManager.LoadScene(newGameLevel);
