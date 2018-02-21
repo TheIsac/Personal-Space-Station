23be7e4c6e83454a0dbbc8219a06b813363bad47
Joakim <jokenordstrom@hotmail.com>
2018-02-21 11:11:38 +0100

    Added mission statement
    at start of game

---
+++ PersonalSpaceStation/Assets/Scripts/MissionStatement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MissionStatement : MonoBehaviour {
+
+    public GameObject missionStatementUI;
+
+    private void Start()
+    {
+        missionStatementUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void PressStartButton()
+    {
+        missionStatementUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
